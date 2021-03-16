17098f46eb62436826c0edfb800ed9dc7579b40f
gunny6026 <gunny6026@naver.com>
2021-03-16 10:34:10 +0900

    상속 관계 매핑 & @MappedSuperclass

---
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "ORDERS")
+public class Order extends BaseEntity {
+
+    @Id @GeneratedValue
+    @Column(name = "ORDER_ID")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+//    @Column(name = "MEMBER_ID")
+//    private Long memberId;
+
+    @OneToOne
+    @JoinColumn(name = "DELIVERY_ID")
+    private Delivery delivery;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+
+}
